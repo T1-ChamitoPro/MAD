cf61154e2c8373e1985f1ed30b5dca9cc85a98d0
Francisco <franciscosampayo2503@gmail.com>
2026-07-15 16:28:26 -0500

    core

---
+++ frontend/MAD/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-neutral-900">
+      <h1 className="text-4xl font-bold text-orange-500">
+        Restaurant App 🍖
+      </h1>
+    </div>
+  );
+}
+
+export default App;
